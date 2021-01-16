bf0e9df0318af101f88deaca53892e4c1ee3defb
Yun Hao <yunhao1@sarlacc.pmacs.upenn.edu>
2021-01-16 16:18:19 -0500

    Remove duplicate descriptor features

--- src/combine_descriptors.R
+++ src/combine_descriptors.R
@@ -19,21 +19,26 @@ feature_types <- as.character(input_df$feature_type);
 feature_files <- as.character(input_df$file);
 
 ## 2.Perform quality control on the dataset
-# Checks the quality of computed descriptors based on the proportion of NA's  
+# checks the quality of computed descriptors based on the proportion of NA's  
 na_ratio <- as.numeric(na_ratio);
 data_df_list <- lapply(feature_files, function(ff) check.descriptors.quality(ff, na_ratio)); 
-# Add descriptor type as prefix of column names 
+# add descriptor type as prefix of column names 
 data_df_list <- mapply(function(ddl, ft){
 	colnames(ddl) <- sapply(colnames(ddl), function(cd) paste(ft, cd, sep = "_"));
 	return(ddl);
 }, data_df_list, feature_types, SIMPLIFY = F);
-# Select descriptors with complete data 
+# select descriptors with complete data 
 sample_list <- lapply(data_df_list, rownames);
 samples <- unlist(sample_list);
 sample_table <- table(samples);
 inter_samples <- names(sample_table)[which(sample_table == length(feature_types))];
 data_df_list <- lapply(data_df_list, function(ddl) ddl[inter_samples,]);
 combine_df <- do.call(cbind, data_df_list);
+# remove duplicate features 
+feat_type <- sapply(colnames(combine_df), function(ccd) strsplit(ccd, "_", fixed = T)[[1]][[2]]);
+uni_feat_type <- unique(feat_type);
+uft_id <- sapply(uni_feat_type, function(uft) which(feat_type %in% uft)[[1]]);
+combine_df <- combine_df[, sort(uft_id)];
 
 ## 3. Output combined descriptors 
 write.table(combine_df, file = combine_output, sep = "\t", row.names = T, col.names = T, quote = F);
